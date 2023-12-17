2ca91242293802feb8900159bf0fa950cac59af7
NicholasTheisen <nicholas.theisen1997@gmail.com>
2023-12-17 06:03:06 +0000

    Breed Fetcher V1.0

---
+++ breedFetcher.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+const fetchBreedDescription = function(breedName) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log('error:', error);
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      console.log('Breed not found');
+    } else {
+      console.log(data[0].description);
+    }
+  });
+};
+
+const breedName = process.argv[2];
+fetchBreedDescription(breedName);
